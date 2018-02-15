d8004bea4dff78061373af767738051e246ebae8
Dante Planterose <dplanterose@gmail.com>
2018-02-15 09:16:24 +0100

    set default value to 0

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,13 @@ class Team < ApplicationRecord
   belongs_to :division
   has_many :players
   validates :name, presence: true
+  after_initialize :init
+
+    def init
+      self.score  ||= 0        #will set the default value only if it's nil
+      self.played ||= 0
+      self.rank ||= 0#let's you set a default association
+    end
 
   # find an issue to validate there is no more than 2 players by team
 end
